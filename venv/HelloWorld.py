5d1fe65923320309fac24107230389531dae0dc2
Dipak <dipaksutare007@gmail.com>
2021-01-05 21:37:48 +0530

    test test

---
+++ venv/HelloWorld.py
@@ -0,0 +1,50 @@
+#print("hello World")
+
+for i in range(0,10,2):
+    for j in range(0,10,2):
+        print(1 + j,end="")
+    print("\n")
+
+#-------------------------------------------------
+
+for i in range(1,6):
+    for j in range(1,6):
+        if (5/j==5 or 5/j==1):
+            print("$",end="")
+        else:
+            print("*",end="")
+    print("\n")
+
+
+#---------------------------
+for i in range(1,6):
+    for j in range(1,6):
+        if (j==3):
+            print("$",end="")
+        else:
+            print("*",end="")
+    print("\n")
+#-------------------
+
+for i in range(1,6):
+    for j in range(1,6):
+        if(i==3):
+            print("$",end="")
+        else:
+            print("*",end="")
+    print("\n")
+
+
+print("---------------10-----------------")
+
+for i in range(1,6):
+    for j in range(1,6):
+        if (5/i==5 or 5/i==1):
+            print("$",end="")
+        else:
+            print("*",end="")
+    print("\n")
+
+
+
+
